c6bc6020a3a29f74c73564e4420689c10ecf1b13
Ali Madani <seyedali.madani@ucalgary.ca>
2023-02-28 16:06:34 -0700

    theme color changed to green

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import customtkinter
 customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+customtkinter.set_default_color_theme("green")
 root=customtkinter.CTk()
 root.geometry("500x350")
 
@@ -17,7 +17,7 @@ entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Threshold")
 entry1.pack (pady=12, padx=10)
 entry2 = customtkinter.CTkComboBox(master=frame,values=["Raw", "AverageBlur", "Gaussian","MedianBlur"])
 entry2.pack (pady=12, padx=10)
-button = customtkinter.CTkButton(master=frame, text="Generate", command=login)
+button = customtkinter.CTkButton(master=frame, text="Generate")
 button.pack (pady=12, padx=10)
 checkbox = customtkinter.CTkCheckBox(master=frame, text="Save After Generation")
 checkbox.pack (pady=12, padx=10)
